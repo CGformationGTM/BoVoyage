7ae15a4b1114b7c7b888c91a05bbaf0eb859a1f7
adminl <adminl@GTM07.home>
2015-07-10 15:17:33 +0200

    derniers reglages

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,16 +1,12 @@
 var affich = false;
-var enFocus = false;
+var affichId = false;
+var click = false;
 
 $(document).ready(function(){
 	$("#destination, #voyage").mouseover(dessus);
 	$("#destination, #voyage").mouseleave(pasDessus);
-	$("#identification").mouseover(mouseEnterId);
-	$("#identification").mouseleave(mouseOutId);
-	$("#connexion > input").focusin(function(){enFocus=true;});
-	$("#connexion > input").focusout(function(){
-		enFocus=false;
-		mouseOutId();
-		});
+	$("#identification").click(mouseEnterId);
+	$("#ok").click(connexion);
 });
 
 function dessus(){
@@ -30,22 +26,29 @@ function pasDessus(){
 }
 
 function mouseEnterId(){
-	if(!affich){
+	if(!affichId){
+		$("#user").focus();
 		$(this).animate({ width : 215, height : 30},100);
 		$(this).css("border-radius", "0 0 0 0");
 		$("#connexion").slideToggle(true);
-		affich=true;
+		affichId=true;
 	}
 }
 
-function mouseOutId(){
-	if(enFocus){
-		return;
-	}
-	if(affich){
-		$(this).animate({ width : 198, height : 20},100);
-		$(this).css("border-radius", "0 0 0 5px");
-		$("#connexion").slideToggle(false);
-		affich=false;
+//function mouseOutId(){
+//	$(this).animate({ width : 198, height : 20},100);
+//	$(this).css("border-radius", "0 0 0 5px");
+//	$("#connexion").slideToggle(false);
+//}
+
+function connexion(){
+	var user=$("#user").val();
+	var pswd=$("#pswd").val();
+	$("#connexion").toggle(false);
+	$(this).animate({ width : 198, height : 20},100);
+	$(this).css("border-radius", "0 0 0 5px");
+	if (user == "Kineton" && pswd == "gencivesdeporc"){
+		$("#empty").empty();
+		$("#empty").html("Bonjour, Simon Jeremy");
 	}
 }
